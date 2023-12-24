98e5507bd5497387e7a177d40d18c39df1c94f5c
lavanyaguru17 <109407476+lavanyaguru17@users.noreply.github.com>
2023-12-24 22:02:13 +0530

    Update main.js

--- Task1-PortfolioWebsite/main.js
+++ Task1-PortfolioWebsite/main.js
@@ -1,7 +1,7 @@
-var typed=new Typed(".text",{
-    strings:["Frontend Developer" , "Backend Developer" , "Web Developer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
+var typed = new Typed(".text", {
+  strings: ["Full stack Developer", "Java Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
